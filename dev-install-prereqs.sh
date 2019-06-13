d7c3cb8e6d4fe71134f127adda272d549cb8306c
mikedarcy <mdarcy@isi.edu>
2019-06-13 15:12:00 -0700

    Adjust requirements for modular deriva-catalog-manage. Update version for 0.5.1 release.

--- dev-install-prereqs.sh
+++ dev-install-prereqs.sh
@@ -3,4 +3,4 @@
 pip install urllib3==1.24.3 markdown bdbag[boto,globus] git+https://github.com/anthony-tuininga/cx_Freeze.git
 pip install git+https://github.com/informatics-isi-edu/deriva-py.git
 pip install git+https://github.com/informatics-isi-edu/deriva-qt.git#egg=deriva-qt[PyQt5]
-pip install git+https://github.com/informatics-isi-edu/deriva-catalog-manage.git
+pip install git+https://github.com/informatics-isi-edu/deriva-catalog-manage.git#egg=deriva-catalog-manage[csv]
--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 import sys
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 if __name__ == '__main__':
     sys.exit(print(__version__))
