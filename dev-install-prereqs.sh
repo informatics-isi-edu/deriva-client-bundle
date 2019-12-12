e56d4b7749400f07323da1d3d7d00fbc112141ae
mikedarcy <mdarcy@isi.edu>
2019-12-11 16:13:31 -0800

    Bump version and dependency versions for release.

--- dev-install-prereqs.sh
+++ dev-install-prereqs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pip install urllib3==1.24.3 markdown bdbag[boto,globus] fair-research-login git+https://github.com/anthony-tuininga/cx_Freeze.git@9e06b761740a9e93431ee7ea8d0b10f786446a6a
+pip install markdown bdbag[boto,globus] fair-research-login git+https://github.com/anthony-tuininga/cx_Freeze.git@9e06b761740a9e93431ee7ea8d0b10f786446a6a
 pip install git+https://github.com/informatics-isi-edu/deriva-py.git
-pip install git+https://github.com/informatics-isi-edu/deriva-qt.git#egg=deriva-qt[PyQt5]
-pip install git+https://github.com/informatics-isi-edu/deriva-catalog-manage.git#egg=deriva-catalog-manage[csv]
+pip install deriva-qt[PyQt5]@git+https://github.com/informatics-isi-edu/deriva-qt.git
+pip install deriva-catalog-manage[csv]@git+https://github.com/informatics-isi-edu/deriva-catalog-manage.git
--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 import sys
 
-__version__ = "0.6.0"
+__version__ = "1.0.0"
 
 if __name__ == '__main__':
     sys.exit(print(__version__))
