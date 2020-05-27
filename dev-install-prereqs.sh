3a14b7de024d41c2d1ab3da202f6dcb44ce00640
mikedarcy <mdarcy@isi.edu>
2020-05-27 15:30:05 -0700

    Release updates.

--- dev-install-prereqs.sh
+++ dev-install-prereqs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pip install markdown bdbag[boto,globus] fair-research-login git+https://github.com/anthony-tuininga/cx_Freeze.git@9e06b761740a9e93431ee7ea8d0b10f786446a6a
+pip install markdown==3.2.1 bdbag[boto,globus] fair-research-login git+https://github.com/anthony-tuininga/cx_Freeze.git@9e06b761740a9e93431ee7ea8d0b10f786446a6a
 pip install git+https://github.com/informatics-isi-edu/deriva-py.git
 pip install deriva-qt[PyQt5]@git+https://github.com/informatics-isi-edu/deriva-qt.git
 pip install deriva-catalog-manage[csv]@git+https://github.com/informatics-isi-edu/deriva-catalog-manage.git
